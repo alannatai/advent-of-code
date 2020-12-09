db5fdd1c392bba2b21fbe3aaab7d17fb56edd48c
Alanna Tai <alanna.tai@hotmail.com>
2020-12-09 13:23:37 -0500

    Complete day2 part2

--- Day2/day2.js
+++ Day2/day2.js
@@ -1,17 +1,48 @@
 const { input } = require('./constants/constants');
 
+const getFormattedPolicies = (input) => {
+  let policies = [];
+  input.map((policy) => {
+    const dict = {};
+    const count = policy.split(' ')[0];
+    dict.nums = count.split('-');
+    dict.letter = policy.split(':')[0][policy.split(':')[0].length - 1];
+    dict.password = policy.split(': ')[1];
+    policies.push(dict);
+  });
+  return policies;
+};
+
 const run = (array) => {
   let validCount = 0;
-  array.forEach((policy) => {
-    const count = policy.split(' ')[0];
-    const countNums = count.split('-');
-    const letter = policy.split(':')[0][policy.split(':')[0].length - 1];
-    const password = policy.split(': ')[1];
+  const policies = getFormattedPolicies(array);
+  policies.map(({ password, nums, letter }) => {
     let letterCount = 0;
-    password && password.split('').map(passLetter => passLetter === letter && letterCount++)
-    letterCount >= countNums[0] && letterCount <= countNums[1] && validCount++
+    password
+      .split('')
+      .map((passLetter) => passLetter === letter && letterCount++);
+    letterCount >= nums[0] && letterCount <= nums[1] && validCount++;
   });
   return validCount;
 };
 
 console.log(run(input));
+
+const runpt2 = (array) => {
+  let validCount = 0;
+  const policies = getFormattedPolicies(array);
+  policies.map(({ password, nums, letter }) => {
+    const lettersArray = password.split('');
+    let matchCount = 0;
+    if (lettersArray[parseInt(nums[0]) - 1] === letter) {
+      matchCount++;
+    }
+    if (lettersArray[parseInt(nums[1]) - 1] === letter) {
+      matchCount++;
+    }
+    matchCount === 1 && validCount++;
+  });
+  return validCount;
+};
+
+console.log(runpt2(input));
